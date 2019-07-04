1b2391c364bfb92ec8ccb26314f2c0e909e426f4
Mallikarjun <mallikarjun.kalyani@dikshatech.com>
2019-07-04 15:33:31 +0530

    first commit

---
+++ src/DemoTest.java
@@ -0,0 +1,30 @@
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.remote.MobileCapabilityType;
+
+public class DemoTest {
+
+	
+	
+public static void main(String[] args) throws Exception {
+		
+		//Set the Desired Capabilities
+/*	File f=new File("src");
+	File fs=new File(f,"PESU_2019-06-24.apk");
+		DesiredCapabilities caps = new DesiredCapabilities();
+		caps.setCapability(MobileCapabilityType.DEVICE_NAME, "Second");
+		caps.setCapability(MobileCapabilityType.APP,fs.getAbsolutePath()); //Give Device ID of your mobile phone
+
+		AndroidDriver<AndroidElement> driver=new AndroidDriver<>(new URL("http://127.0.0.1:4723/wd/hub"),caps);
+	*/
+		
+}
+}
